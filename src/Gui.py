0344276b3b6a410044981199e6ebc0d31ee5b704
YyarosS <kazak.yaroslav.2005@gmail.com>
2025-11-19 11:16:29 +0300

    main_template

--- src/Gui.py
+++ src/Gui.py
@@ -1,15 +1,17 @@
-import tkinter
 import customtkinter
+import tkinter
+from tkinter import ttk
 
-class Welcome_window(customtkinter.CTk):
+class Main_window(customtkinter.CTk):
     def __init__(self,window_resolution):
         super().__init__()
         self.__window_resolution = self.set_resolution(window_resolution)
         self.geometry(self.__window_resolution)
+        self.configure(fg_color = "#a564df")
         self.minsize(640,480)
         self.maxsize(1920,1080)
 
-
+        self.add_background(self, 1000, 500, "#0e0f11",8, "#625b9f", 6)
     @staticmethod
     def is_resolution(resolution):
         parts = resolution.split('x')
@@ -27,6 +29,19 @@ class Welcome_window(customtkinter.CTk):
             self.__window_resolution = window_resolution
         else :
             print("This is not resolution")
-
-    def get_resolution(self):
-        return self.__window_resolution
+    
+    def add_background(self,node, width, height, color, border_width, border_color, corner_radius):
+        background = customtkinter.CTkFrame(
+            master = node,
+            width=width,
+            height= height,
+            fg_color=color,
+            border_width=border_width,
+            border_color=border_color,
+            corner_radius=corner_radius)
+        background.place(
+            anchor = "c",
+            relx =.5,
+            rely =.5,
+            relwidth = 1.1,
+            relheight = .85)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,37 @@
-from Gui import Welcome_window
+from Gui import Main_window
+import customtkinter
+import tkinter
+import os
+
+def create_font():
+    font_family = "Hikasami-Bold.ttf"
+    project_path = "~/Документы/Учеба/3 семестр/Open Source/GitUpdChecky/UpdChecky"
+    font_path = os.path.join(project_path, font_family)
+    font_size = 36,
+    font_weight= "Bold"
+    try:
+        title_font = customtkinter.CTkFont(
+            family = font_family,
+            size = font_size,
+            weight=font_weight,
+            file = font_path)
+    except Exception as ex:
+        print("Ошибка загрузки шрифта")
+    return title_font
 
 window_resolution = "800x600"
 
-first_windows = Welcome_window(window_resolution)
-first_windows.title("")
-first_windows.mainloop()
+main_windows = Main_window(window_resolution)
+main_windows.title("")
+
+title_text = "Welcome to UpdChecky"
+text_color = "#a564df"
+
+title = customtkinter.CTkLabel(
+    master=main_windows,
+    text=title_text,
+    text_color=text_color,
+    font= create_font())
+title.place(anchor = "c", relx = 0.5, rely = 0.3)
+
+main_windows.mainloop()
